bc528496c349021941c2bc19f67e178813a2bede
x_zhuo <12474586+zx06@users.noreply.github.com>
2021-06-01 02:27:46 +0000

    add devcontainer

--- go.mod
+++ go.mod
@@ -1,5 +1,11 @@
 module gin
 
-go 1.14
+go 1.16
 
-require github.com/gin-gonic/gin v1.7.1 // indirect
+require (
+	github.com/gin-contrib/expvar v0.0.1
+	github.com/gin-contrib/requestid v0.0.1
+	github.com/gin-contrib/zap v0.0.1
+	github.com/gin-gonic/gin v1.7.2
+	go.uber.org/zap v1.17.0
+)
--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/expvar"
+	"github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/requestid"
+	"go.uber.org/zap"
 )
-var Router * gin.Engine
+
 func main() {
-	r := gin.Default()
+	r := gin.New()
+	logger, _ := zap.NewProduction()
+
+	r.Use(requestid.New())
+
+	r.Use(ginzap.Ginzap(logger, time.RFC3339, false))
+	r.Use(ginzap.RecoveryWithZap(logger, true))
+
+	r.GET("/debug/vars", expvar.Handler())
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello world!",
